d9a6e6c54ea3dba72e55148833ad4659d5eb33ee
Emily <eschlaf2@illinois.edu>
2016-03-30 00:04:51 -0500

    Updating notes

--- HCP/Results/agg_slm/make_slm_hist.m
+++ HCP/Results/agg_slm/make_slm_hist.m
@@ -1,7 +1,8 @@
-N = 33;
+N = 5;
 
+n = size(slm{1},1);
 if ~exist('slm_mat','var')
-    slm_mat = zeros(33,33,200);
+    slm_mat = zeros([n,n,200]);
     for i = (1:200)
         [~,unperm] = sort(perms{i});
         slm_mat(:,:,i) = slm{i}(unperm,unperm);
@@ -9,23 +10,37 @@ if ~exist('slm_mat','var')
 end
 
 mean_slm = mean(slm_mat,3);
+std_slm = std(slm_mat,[],3);
 
 s = HWSetup();
 slm_hist = cell(N,N);
+[i,j] = ndgrid((1:n),(1:n));
+ms = [i(:) j(:) mean_slm(:) std_slm(:)];
+[~,sort_ord] = sort(ms(:,3),'descend');
+sorted_ms = ms(sort_ord,:);
 % centers = linspace(-500,2000,10);
-for i = (1:N-1)
-    for j = (i+1:N)
-        [nelements,centers] = hist(squeeze(slm_mat(i,j,:)));
+
+for dummy_hist_ct = 1:length(ms)
+    record = sorted_ms(dummy_hist_ct,:);
+    i = record(1); j = record(2); m = record(3); v = record(4);
+    [nelements,centers] = hist(squeeze(slm_mat(i,j,:)));
 %         nelements = hist(squeeze(slm_mat(i,j,:)),centers);
-        bar(centers,nelements);
-        ylim([0 100])
-        xlim([-3000,3000])
-        title(['(i,j) = (',num2str(i),',',num2str(j),')']);
-        slm_hist{i,j}.centers = centers;
-        slm_hist{i,j}.nelements = nelements;
-        figid = ['slm_ij',num2str(i,'%02d'),num2str(j,'%02d')];
-        savefig(['fig/',figid])
-        hgexport(gcf,['png/',figid],s);
-    end
+    bar(centers,nelements);
+    ylim([0 100])
+    xlim([-3000,3000])
+    fig_title = sprintf('(i,j) = (%d,%d)',i,j);
+    annot_str = {sprintf('m = %3g',m); sprintf('std = %3g',v)};
+    title(fig_title);
+    pos = get(gca,'position');
+    annotation('textbox',...
+        'position',[pos(1)+.01, pos(2)+pos(4)-.11, .3, .1],...
+        'string',annot_str,...
+        'fitboxtotext','on','linestyle','none')
+    slm_hist{i,j}.centers = centers;
+    slm_hist{i,j}.nelements = nelements;
+    figid = sprintf('slm_%04d_ij%02d%02d',dummy_hist_ct,i,j);
+    savefig(['fig/',figid])
+    hgexport(gcf,['png/',figid],s);
+    close 1
 end
 save('slm_hist','slm_hist');
