c742f51dc1ea8afd5314ec249cb51a7b9fc6c821
Emily <eschlaf2@illinois.edu>
2016-04-20 10:28:55 -0500

    Update notes - add music analysis

--- Notes/pictures/permLocations.m
+++ Notes/pictures/permLocations.m
@@ -1,3 +1,6 @@
+file = dir('cyclicity_all_resting.mat');
+load(file.name);
+group = file.name(11:end-4);
 n = numel(perms);
 r = numel(perms{1});
 perms_mat = cell2mat(perms');
@@ -6,13 +9,16 @@ perm_locs = cell2mat(arrayfun(@(x) sum(perms_mat == x)./n, ...
 imagesc(perm_locs);
 colormap(gray(4));
 colorbar
-loc = strsplit(pwd,'/');
+% loc = strsplit(pwd,'/');
+% title_str = sprintf(...
+%     'Percent of appearances in permutation locations\n%s - %s (n=%d)',...
+%     loc{end-1},loc{end},n);
 title_str = sprintf(...
-    'Percent of appearances in permutation locations\n%s - %s (n=%d)',...
-    loc{end-1},loc{end},n);
+    'Percent of appearances in permutation locations\n%s', group);
 %     f.name,'preictal',n);
 title(title_str,'interpreter','none');
 xlabel('Permutation location');
 ylabel('Region');
-savefig('perm_locations');
-hgexport(gcf,'perm_locations',HWSetup());
+outid = ['perm_locations',file.name(11:end-4)];
+savefig(outid);
+hgexport(gcf,outid,HWSetup());
